afe276fa54b448994bf1a3257ba16b49ff8f1795
Shakir Yakubov <yakuboff.sh@yandex.ru>
2024-11-04 00:09:39 +0300

    fix

--- src/controllers/referralController.mjs
+++ src/controllers/referralController.mjs
@@ -1,9 +1,13 @@
 import Referal from "../models/referralModel.mjs"
 
 export const getReferralsCount = async (req, res) => {
-  console.log("Отдаю реферралов")
-  const refer_id = req.params.id
-  const referrals = await Referal.find({ refer_id })
-  const refCount = referrals.length
-  res.status(200).send({ referralsCount: refCount })
+  try {
+    console.log("Отдаю реферралов")
+    const refer_id = req.params.id
+    const referrals = await Referal.find({ refer_id })
+    const refCount = referrals.length
+    res.status(200).send({ referralsCount: refCount })
+  } catch (e) {
+    console.log("Error while referral count ", e)
+  }
 }
