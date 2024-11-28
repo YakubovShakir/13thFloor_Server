3c80c89c97dd61995842d202a5985ea1113dc549
Shakir Yakubov <yakuboff.sh@yandex.ru>
2024-11-28 15:12:05 +0300

    path fix

--- src/gameTimer/gameTimer.mjs
+++ src/gameTimer/gameTimer.mjs
@@ -1,6 +1,6 @@
 import { FoodProccess } from "./tasks/FoodProcess/task.mjs"
 import { SkillProccess } from "./tasks/SkillProcess/task.mjs"
-import { WorkProccess } from "./tasks/WorkProcess /task.mjs"
+import { WorkProccess } from "./tasks/WorkProcess/task.mjs"
 import { TrainingProccess } from "./tasks/TrainingProcess/task.mjs"
 
 const gameTimer = {
