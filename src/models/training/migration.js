e0867ad1a2ef255f48e2ff0a5616d577bbbd5e0b
Shakir Yakubov <yakuboff.sh@yandex.ru>
2024-12-09 18:09:52 +0300

    add level and training migr

--- src/models/training/migration.js
+++ src/models/training/migration.js
@@ -1,8 +1,5 @@
 export default [
   {
-    _id: {
-      $oid: "673d32f9e9bef24d9c41426e",
-    },
     level: 1,
     duration: 1,
     energy_spend: 0.83,
@@ -10,9 +7,6 @@ export default [
     mood_profit: 2,
   },
   {
-    _id: {
-      $oid: "673d32f9e9bef24d9c41426f",
-    },
     level: 2,
     duration: 5,
     energy_spend: 2,
@@ -20,9 +14,6 @@ export default [
     mood_profit: 4,
   },
   {
-    _id: {
-      $oid: "673d32f9e9bef24d9c414270",
-    },
     level: 3,
     duration: 10,
     energy_spend: 3,
@@ -30,9 +21,6 @@ export default [
     mood_profit: 8,
   },
   {
-    _id: {
-      $oid: "673d32f9e9bef24d9c414271",
-    },
     level: 4,
     duration: 14,
     energy_spend: 4,
@@ -40,9 +28,6 @@ export default [
     mood_profit: 12,
   },
   {
-    _id: {
-      $oid: "673d32f9e9bef24d9c414272",
-    },
     level: 5,
     duration: 18,
     energy_spend: 5,
@@ -50,9 +35,6 @@ export default [
     mood_profit: 14,
   },
   {
-    _id: {
-      $oid: "673d32f9e9bef24d9c414273",
-    },
     level: 6,
     duration: 22,
     energy_spend: 5.83,
@@ -60,9 +42,6 @@ export default [
     mood_profit: 18,
   },
   {
-    _id: {
-      $oid: "673d32f9e9bef24d9c414274",
-    },
     level: 7,
     duration: 27,
     energy_spend: 6.67,
@@ -70,9 +49,6 @@ export default [
     mood_profit: 20,
   },
   {
-    _id: {
-      $oid: "673d32f9e9bef24d9c414275",
-    },
     level: 8,
     duration: 30,
     energy_spend: 7,
@@ -80,9 +56,6 @@ export default [
     mood_profit: 22,
   },
   {
-    _id: {
-      $oid: "673d32f9e9bef24d9c414276",
-    },
     level: 9,
     duration: 35,
     energy_spend: 8,
@@ -90,9 +63,6 @@ export default [
     mood_profit: 28,
   },
   {
-    _id: {
-      $oid: "673d32f9e9bef24d9c414277",
-    },
     level: 10,
     duration: 40,
     energy_spend: 9,
@@ -100,9 +70,6 @@ export default [
     mood_profit: 30,
   },
   {
-    _id: {
-      $oid: "673d32f9e9bef24d9c414278",
-    },
     level: 12,
     duration: 48,
     energy_spend: 11,
@@ -110,9 +77,6 @@ export default [
     mood_profit: 34,
   },
   {
-    _id: {
-      $oid: "673d32f9e9bef24d9c414279",
-    },
     level: 13,
     duration: 50,
     energy_spend: 12,
@@ -120,9 +84,6 @@ export default [
     mood_profit: 36,
   },
   {
-    _id: {
-      $oid: "673d32f9e9bef24d9c41427a",
-    },
     level: 14,
     duration: 55,
     energy_spend: 13,
@@ -130,9 +91,6 @@ export default [
     mood_profit: 38,
   },
   {
-    _id: {
-      $oid: "673d32f9e9bef24d9c41427b",
-    },
     level: 11,
     duration: 43,
     energy_spend: 10,
@@ -140,9 +98,6 @@ export default [
     mood_profit: 32,
   },
   {
-    _id: {
-      $oid: "673d32f9e9bef24d9c41427c",
-    },
     level: 15,
     duration: 60,
     energy_spend: 13.33,
