35eed68fb6e26f6eff799cb9428bb7fd20f2b65a
Leonid Brown <leonid.brown01@gmail.com>
2025-01-19 12:24:35 +0300

    fix: cleaning up user processes to avoid bugs

--- src/models/skill/skillModel.mjs
+++ src/models/skill/skillModel.mjs
@@ -2,13 +2,13 @@ import mongoose from "mongoose"
 
 const skillSchema = new mongoose.Schema({
   name: {
-    ru: { type: String, unique: true, required: true },
-    en: { type: String, unique: true, required: true }
+    ru: { type: String, required: true },
+    en: { type: String, required: true }
   },
   coins_price: { type: Number, required: true },
   description: {
-    ru: { type: String, unique: true, required: false },
-    en: { type: String, unique: true, required: false }
+    ru: { type: String, required: false },
+    en: { type: String, required: false }
   },
   link: { type: String, unique: true },
   duration: { type: Number },
