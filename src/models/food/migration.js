b34c544a1b8f77758958fa0cb7dbb71ae81f2119
Sayyeap <156927818+Sayyeap@users.noreply.github.com>
2025-01-23 13:12:17 +0300

    бек новые цифры

--- src/models/food/migration.js
+++ src/models/food/migration.js
@@ -1,49 +1,22 @@
 export default [
   {
-    user_level_require: 3,
-    
+    user_level_require: 1,
     name: {
-      ru: "Бодрящий ланч",
-      en: "Energy lunch"},
-    duration: 45,
+      ru: "Яблоко",
+      en: "Apple"},    
+    duration: 20,
     instant_energy_restore: null,
-    long_energy_restore: {
-      value: 180,
-      percent: false,
-    },
+    long_energy_restore: null,
     instant_hungry_restore: {
-      value: 1,
+      value: 10,
       percent: true,
     },
     long_hungry_restore: null,
     instant_mood_restore: null,
     long_mood_restore: null,
-    coins_price: 1,
-    link: "https://13floorgame.ru/images/foods/liteLunch.png",
-    food_id: 3,
-  },
-  {
-    user_level_require: 10,
-  
-    name: {
-      ru: "Стейк",
-      en: "Steak"},
-    duration: 50,
-    instant_energy_restore: null,
-    long_energy_restore: null,
-    instant_hungry_restore: {
-      value: 16,
-      percent: true,
-    },
-    long_hungry_restore: {
-      value: 2,
-      percent: true,
-    },
-    instant_mood_restore: null,
-    long_mood_restore: null,
-    coins_price: 1,
-    link: "https://13floorgame.ru/images/foods/steak.png",
-    food_id: 6,
+    coins_price: 5,
+    link: "https://13floorgame.ru/images/foods/apple.png",
+    food_id: 1,
   },
   {
     user_level_require: 2,
@@ -59,98 +32,37 @@ export default [
       percent: true,
     },
     long_hungry_restore: {
-      value: 0.5,
+      value: 15,
       percent: true,
     },
     instant_mood_restore: null,
     long_mood_restore: null,
-    coins_price: 1,
+    coins_price: 25,
     link: "https://13floorgame.ru/images/foods/burger.png",
     food_id: 2,
   },
   {
-    user_level_require: 14,
+    user_level_require: 3,
     
     name: {
-      ru: "Буррито",
-      en: "Burrito"},
+      ru: "Бодрящий ланч",
+      en: "Energy lunch"},
     duration: 45,
     instant_energy_restore: null,
     long_energy_restore: {
-      value: 50,
+      value: 180,
       percent: false,
     },
     instant_hungry_restore: {
-      value: 25,
-      percent: true,
-    },
-    long_hungry_restore: null,
-    instant_mood_restore: null,
-    long_mood_restore: null,
-    coins_price: 1,
-    link: "https://13floorgame.ru/images/foods/burrito.png",
-    food_id: 8,
-  },
-  {
-    user_level_require: 1,
-    name: {
-      ru: "Яблоко",
-      en: "Apple"},    
-    duration: 20,
-    instant_energy_restore: null,
-    long_energy_restore: null,
-    instant_hungry_restore: {
-      value: 1.5,
-      percent: true,
-    },
-    long_hungry_restore: null,
-    instant_mood_restore: null,
-    long_mood_restore: null,
-    coins_price: 1,
-    link: "https://13floorgame.ru/images/foods/apple.png",
-    food_id: 1,
-  },
-  {
-    user_level_require: 12,
-    name: {
-      ru: "Суши",
-      en: "Sushi"},
-    duration: 200,
-    instant_energy_restore: null,
-    long_energy_restore: null,
-    instant_hungry_restore: {
-      value: 7,
-      percent: true,
-    },
-    long_hungry_restore: null,
-    instant_mood_restore: null,
-    long_mood_restore: {
       value: 5,
       percent: true,
     },
-    coins_price: 1,
-    link: "https://13floorgame.ru/images/foods/sushi.png",
-    food_id: 7,
-  },
-  {
-    user_level_require: 6,
-    
-    name: {
-      ru: "Протеиновый батончик",
-      en: "Protein Bar"},
-    duration: 120,
-    instant_energy_restore: null,
-    long_energy_restore: null,
-    instant_hungry_restore: {
-      value: 15,
-      percent: true,
-    },
     long_hungry_restore: null,
     instant_mood_restore: null,
     long_mood_restore: null,
-    coins_price: 1,
-    link: "https://13floorgame.ru/images/foods/proteinBar.png",
-    food_id: 5,
+    coins_price: 50,
+    link: "https://13floorgame.ru/images/foods/liteLunch.png",
+    food_id: 3,
   },
   {
     user_level_require: 4,
@@ -162,17 +74,107 @@ export default [
     instant_energy_restore: null,
     long_energy_restore: null,
     instant_hungry_restore: {
-      value: 10,
+      value: 25,
       percent: true,
     },
     long_hungry_restore: {
-      value: 1.5,
+      value: 15,
       percent: true,
     },
     instant_mood_restore: null,
     long_mood_restore: null,
-    coins_price: 1,
+    coins_price: 80,
     link: "https://13floorgame.ru/images/foods/lunch.png",
     food_id: 4,
   },
+  {
+    user_level_require: 6,
+    
+    name: {
+      ru: "Протеиновый батончик",
+      en: "Protein Bar"},
+    duration: 120,
+    instant_energy_restore: null,
+    long_energy_restore: null,
+    instant_hungry_restore: {
+      value: 35,
+      percent: true,
+    },
+    long_hungry_restore: null,
+    instant_mood_restore: null,
+    long_mood_restore: null,
+    coins_price: 150,
+    link: "https://13floorgame.ru/images/foods/proteinBar.png",
+    food_id: 5,
+  },
+  {
+    user_level_require: 10,
+  
+    name: {
+      ru: "Стейк",
+      en: "Steak"},
+    duration: 50,
+    instant_energy_restore: null,
+    long_energy_restore: null,
+    instant_hungry_restore: {
+      value: 20,
+      percent: true,
+    },
+    long_hungry_restore: {
+      value: 18,
+      percent: true,
+    },
+    instant_mood_restore: null,
+    long_mood_restore: null,
+    coins_price: 200,
+    link: "https://13floorgame.ru/images/foods/steak.png",
+    food_id: 6,
+  },
+  
+  {
+    user_level_require: 12,
+    name: {
+      ru: "Суши",
+      en: "Sushi"},
+    duration: 200,
+    instant_energy_restore: null,
+    long_energy_restore: null,
+    instant_hungry_restore: {
+      value: 40,
+      percent: true,
+    },
+    long_hungry_restore: null,
+    instant_mood_restore: null,
+    long_mood_restore: {
+      value: 7,
+      percent: true,
+    },
+    coins_price: 380,
+    link: "https://13floorgame.ru/images/foods/sushi.png",
+    food_id: 7,
+  },
+  {
+    user_level_require: 14,
+    
+    name: {
+      ru: "Буррито",
+      en: "Burrito"},
+    duration: 45,
+    instant_energy_restore: null,
+    long_energy_restore: {
+      value: 500,
+      percent: false,
+    },
+    instant_hungry_restore: {
+      value: 25,
+      percent: true,
+    },
+    long_hungry_restore: null,
+    instant_mood_restore: null,
+    long_mood_restore: null,
+    coins_price: 450,
+    link: "https://13floorgame.ru/images/foods/burrito.png",
+    food_id: 8,
+  },
+ 
 ]
