8d8f6d155c6a6b859ad63192bdd910db50a625bb
Sayyeap <156927818+Sayyeap@users.noreply.github.com>
2025-01-24 15:02:13 +0300

    миграшка респект

--- src/models/investments/migration.js
+++ src/models/investments/migration.js
@@ -2,7 +2,7 @@ export default [
   {
     id: 1,
     coins_per_hour: 50,
-    respect: 100,
+    respect: 5,
     level: 1,
     price: 0,
     type: "game_center",
@@ -14,7 +14,7 @@ export default [
   {
     id: 2,
     coins_per_hour: 250,
-    respect: 200,
+    respect: 10,
     level: 2,
     price: 0,
     type: "game_center",
@@ -26,7 +26,7 @@ export default [
   {
     id: 3,
     coins_per_hour: 500,
-    respect: 300,
+    respect: 10,
     level: 3,
     price: 0,
     type: "game_center",
@@ -38,7 +38,7 @@ export default [
   {
     id: 4,
     coins_per_hour: 1000,
-    respect: 400,
+    respect: 40,
     level: 4,
     price: 0,
     type: "game_center",
@@ -50,7 +50,7 @@ export default [
   {
     id: 5,
     coins_per_hour: 1150,
-    respect: 500,
+    respect: 50,
     level: 5,
     price: 0,
     type: "game_center",
@@ -62,7 +62,7 @@ export default [
   {
     id: 6,
     coins_per_hour: 1300,
-    respect: 550,
+    respect: 50,
     level: 6,
     price: 0,
     type: "game_center",
@@ -74,7 +74,7 @@ export default [
   {
     id: 7,
     coins_per_hour: 1450,
-    respect: 600,
+    respect: 40,
     level: 7,
     price: 0,
     type: "game_center",
@@ -86,7 +86,7 @@ export default [
   {
     id: 8,
     coins_per_hour: 1600,
-    respect: 650,
+    respect: 35,
     level: 8,
     price: 0,
     type: "game_center",
@@ -98,7 +98,7 @@ export default [
   {
     id: 9,
     coins_per_hour: 1750,
-    respect: 700,
+    respect: 35,
     level: 9,
     price: 0,
     type: "game_center",
@@ -110,7 +110,7 @@ export default [
   {
     id: 10,
     coins_per_hour: 1900,
-    respect: 750,
+    respect: 35,
     level: 10,
     price: 0,
     type: "game_center",
@@ -122,7 +122,7 @@ export default [
   {
     id: 11,
     coins_per_hour: 2050,
-    respect: 800,
+    respect: 10,
     level: 11,
     price: 0,
     type: "game_center",
@@ -134,7 +134,7 @@ export default [
   {
     id: 12,
     coins_per_hour: 2200,
-    respect: 850,
+    respect: 10,
     level: 12,
     price: 0,
     type: "game_center",
@@ -146,7 +146,7 @@ export default [
   {
     id: 13,
     coins_per_hour: 2350,
-    respect: 900,
+    respect: 10,
     level: 13,
     price: 0,
     type: "game_center",
@@ -158,7 +158,7 @@ export default [
   {
     id: 14,
     coins_per_hour: 2500,
-    respect: 950,
+    respect: 10,
     level: 14,
     price: 0,
     type: "game_center",
@@ -170,7 +170,7 @@ export default [
   {
     id: 15,
     coins_per_hour: 2650,
-    respect: 1000,
+    respect: 10,
     level: 15,
     price: 0,
     type: "game_center",
@@ -182,7 +182,7 @@ export default [
   {
     id: 16,
     coins_per_hour: 2800,
-    respect: 1050,
+    respect: 10,
     level: 16,
     price: 0,
     type: "game_center",
@@ -194,7 +194,7 @@ export default [
   {
     id: 17,
     coins_per_hour: 2950,
-    respect: 1100,
+    respect: 10,
     level: 17,
     price: 0,
     type: "game_center",
@@ -206,7 +206,7 @@ export default [
   {
     id: 18,
     coins_per_hour: 3100,
-    respect: 1150,
+    respect: 10,
     level: 18,
     price: 0,
     type: "game_center",
@@ -218,7 +218,7 @@ export default [
   {
     id: 19,
     coins_per_hour: 3250,
-    respect: 1200,
+    respect: 10,
     level: 19,
     price: 0,
     type: "game_center",
@@ -230,7 +230,7 @@ export default [
   {
     id: 20,
     coins_per_hour: 3400,
-    respect: 1250,
+    respect: 10,
     level: 20,
     price: 0,
     type: "game_center",
@@ -242,7 +242,7 @@ export default [
   {
     id: 21,
     coins_per_hour: 3550,
-    respect: 1300,
+    respect: 10,
     level: 21,
     price: 0,
     type: "game_center",
@@ -254,7 +254,7 @@ export default [
   {
     id: 22,
     coins_per_hour: 3700,
-    respect: 1350,
+    respect: 10,
     level: 22,
     price: 0,
     type: "game_center",
@@ -266,7 +266,7 @@ export default [
   {
     id: 23,
     coins_per_hour: 3850,
-    respect: 1400,
+    respect: 10,
     level: 23,
     price: 0,
     type: "game_center",
@@ -278,7 +278,7 @@ export default [
   {
     id: 24,
     coins_per_hour: 4000,
-    respect: 1450,
+    respect: 10,
     level: 24,
     price: 0,
     type: "game_center",
@@ -290,7 +290,7 @@ export default [
   {
     id: 25,
     coins_per_hour: 4150,
-    respect: 1500,
+    respect: 10,
     level: 25,
     price: 0,
     type: "game_center",
@@ -302,7 +302,7 @@ export default [
   {
     id: 26,
     coins_per_hour: 4300,
-    respect: 1550,
+    respect: 10,
     level: 26,
     price: 0,
     type: "game_center",
@@ -314,7 +314,7 @@ export default [
   {
     id: 27,
     coins_per_hour: 4450,
-    respect: 1600,
+    respect: 10,
     level: 27,
     price: 0,
     type: "game_center",
@@ -326,7 +326,7 @@ export default [
   {
     id: 28,
     coins_per_hour: 4600,
-    respect: 1650,
+    respect: 10,
     level: 28,
     price: 0,
     type: "game_center",
@@ -338,7 +338,7 @@ export default [
   {
     id: 29,
     coins_per_hour: 4750,
-    respect: 1700,
+    respect: 10,
     level: 29,
     price: 0,
     type: "game_center",
@@ -350,7 +350,7 @@ export default [
   {
     id: 30,
     coins_per_hour: 4900,
-    respect: 1750,
+    respect: 10,
     level: 30,
     price: 0,
     type: "game_center",
@@ -362,7 +362,7 @@ export default [
   {
     id: 31,
     coins_per_hour: 5050,
-    respect: 1800,
+    respect: 10,
     level: 31,
     price: 0,
     type: "game_center",
@@ -374,7 +374,7 @@ export default [
   {
     id: 32,
     coins_per_hour: 5200,
-    respect: 1850,
+    respect: 10,
     level: 32,
     price: 0,
     type: "game_center",
@@ -386,7 +386,7 @@ export default [
   {
     id: 33,
     coins_per_hour: 5350,
-    respect: 1900,
+    respect: 10,
     level: 33,
     price: 0,
     type: "game_center",
@@ -398,7 +398,7 @@ export default [
   {
     id: 34,
     coins_per_hour: 5500,
-    respect: 1950,
+    respect: 10,
     level: 34,
     price: 0,
     type: "game_center",
@@ -410,7 +410,7 @@ export default [
   {
     id: 35,
     coins_per_hour: 5650,
-    respect: 2000,
+    respect: 10,
     level: 35,
     price: 0,
     type: "game_center",
@@ -422,9 +422,9 @@ export default [
   {
     id: 36,
     coins_per_hour: 150,
-    respect: 100,
+    respect: 10,
     level: 1,
-    price: 300,
+    price: 800,
     type: "coffee_shop",
     title: {
       ru: "Кофейня",
@@ -434,7 +434,7 @@ export default [
   {
     id: 37,
     coins_per_hour: 200,
-    respect: 200,
+    respect: 10,
     level: 2,
     price: 450,
     type: "coffee_shop",
@@ -446,7 +446,7 @@ export default [
   {
     id: 38,
     coins_per_hour: 250,
-    respect: 300,
+    respect: 10,
     level: 3,
     price: 680,
     type: "coffee_shop",
@@ -458,7 +458,7 @@ export default [
   {
     id: 39,
     coins_per_hour: 300,
-    respect: 400,
+    respect: 10,
     level: 4,
     price: 1020,
     type: "coffee_shop",
@@ -470,7 +470,7 @@ export default [
   {
     id: 40,
     coins_per_hour: 350,
-    respect: 500,
+    respect: 10,
     level: 5,
     price: 1530,
     type: "coffee_shop",
@@ -482,7 +482,7 @@ export default [
   {
     id: 41,
     coins_per_hour: 400,
-    respect: 550,
+    respect: 10,
     level: 6,
     price: 2300,
     type: "coffee_shop",
@@ -494,7 +494,7 @@ export default [
   {
     id: 42,
     coins_per_hour: 450,
-    respect: 600,
+    respect: 10,
     level: 7,
     price: 3450,
     type: "coffee_shop",
@@ -506,7 +506,7 @@ export default [
   {
     id: 43,
     coins_per_hour: 500,
-    respect: 650,
+    respect: 10,
     level: 8,
     price: 5180,
     type: "coffee_shop",
@@ -518,7 +518,7 @@ export default [
   {
     id: 44,
     coins_per_hour: 550,
-    respect: 700,
+    respect: 10,
     level: 9,
     price: 7770,
     type: "coffee_shop",
@@ -530,7 +530,7 @@ export default [
   {
     id: 45,
     coins_per_hour: 600,
-    respect: 750,
+    respect: 10,
     level: 10,
     price: 11660,
     type: "coffee_shop",
@@ -542,7 +542,7 @@ export default [
   {
     id: 46,
     coins_per_hour: 650,
-    respect: 800,
+    respect: 10,
     level: 11,
     price: 17490,
     type: "coffee_shop",
@@ -554,7 +554,7 @@ export default [
   {
     id: 47,
     coins_per_hour: 700,
-    respect: 850,
+    respect: 10,
     level: 12,
     price: 26240,
     type: "coffee_shop",
@@ -566,7 +566,7 @@ export default [
   {
     id: 48,
     coins_per_hour: 750,
-    respect: 900,
+    respect: 10,
     level: 13,
     price: 39360,
     type: "coffee_shop",
@@ -578,7 +578,7 @@ export default [
   {
     id: 49,
     coins_per_hour: 800,
-    respect: 950,
+    respect: 10,
     level: 14,
     price: 47230,
     type: "coffee_shop",
@@ -590,7 +590,7 @@ export default [
   {
     id: 50,
     coins_per_hour: 850,
-    respect: 1000,
+    respect: 10,
     level: 15,
     price: 56680,
     type: "coffee_shop",
@@ -602,7 +602,7 @@ export default [
   {
     id: 51,
     coins_per_hour: 900,
-    respect: 1050,
+    respect: 10,
     level: 16,
     price: 68020,
     type: "coffee_shop",
@@ -614,7 +614,7 @@ export default [
   {
     id: 52,
     coins_per_hour: 950,
-    respect: 1100,
+    respect: 10,
     level: 17,
     price: 81620,
     type: "coffee_shop",
@@ -626,7 +626,7 @@ export default [
   {
     id: 53,
     coins_per_hour: 1000,
-    respect: 1150,
+    respect: 10,
     level: 18,
     price: 97940,
     type: "coffee_shop",
@@ -638,7 +638,7 @@ export default [
   {
     id: 54,
     coins_per_hour: 1070,
-    respect: 1200,
+    respect: 10,
     level: 19,
     price: 117530,
     type: "coffee_shop",
@@ -650,7 +650,7 @@ export default [
   {
     id: 55,
     coins_per_hour: 1140,
-    respect: 1250,
+    respect: 10,
     level: 20,
     price: 129280,
     type: "coffee_shop",
@@ -662,7 +662,7 @@ export default [
   {
     id: 56,
     coins_per_hour: 1210,
-    respect: 1300,
+    respect: 10,
     level: 21,
     price: 142210,
     type: "coffee_shop",
@@ -674,7 +674,7 @@ export default [
   {
     id: 57,
     coins_per_hour: 1280,
-    respect: 1350,
+    respect: 10,
     level: 22,
     price: 156430,
     type: "coffee_shop",
@@ -686,7 +686,7 @@ export default [
   {
     id: 58,
     coins_per_hour: 1350,
-    respect: 1400,
+    respect: 10,
     level: 23,
     price: 172070,
     type: "coffee_shop",
@@ -698,7 +698,7 @@ export default [
   {
     id: 59,
     coins_per_hour: 1420,
-    respect: 1450,
+    respect: 10,
     level: 24,
     price: 189280,
     type: "coffee_shop",
@@ -710,7 +710,7 @@ export default [
   {
     id: 60,
     coins_per_hour: 1490,
-    respect: 1500,
+    respect: 10,
     level: 25,
     price: 208210,
     type: "coffee_shop",
@@ -722,7 +722,7 @@ export default [
   {
     id: 61,
     coins_per_hour: 1590,
-    respect: 1550,
+    respect: 10,
     level: 26,
     price: 229030,
     type: "coffee_shop",
@@ -734,7 +734,7 @@ export default [
   {
     id: 62,
     coins_per_hour: 1700,
-    respect: 1600,
+    respect: 10,
     level: 27,
     price: 251930,
     type: "coffee_shop",
@@ -746,7 +746,7 @@ export default [
   {
     id: 63,
     coins_per_hour: 1810,
-    respect: 1650,
+    respect: 10,
     level: 28,
     price: 277120,
     type: "coffee_shop",
@@ -758,7 +758,7 @@ export default [
   {
     id: 64,
     coins_per_hour: 1920,
-    respect: 1700,
+    respect: 10,
     level: 29,
     price: 304830,
     type: "coffee_shop",
@@ -770,7 +770,7 @@ export default [
   {
     id: 65,
     coins_per_hour: 2030,
-    respect: 1750,
+    respect: 10,
     level: 30,
     price: 335310,
     type: "coffee_shop",
@@ -782,7 +782,7 @@ export default [
   {
     id: 66,
     coins_per_hour: 2140,
-    respect: 1800,
+    respect: 10,
     level: 31,
     price: 368840,
     type: "coffee_shop",
@@ -794,7 +794,7 @@ export default [
   {
     id: 67,
     coins_per_hour: 2250,
-    respect: 1850,
+    respect: 10,
     level: 32,
     price: 405720,
     type: "coffee_shop",
@@ -806,7 +806,7 @@ export default [
   {
     id: 68,
     coins_per_hour: 2360,
-    respect: 1900,
+    respect: 10,
     level: 33,
     price: 446290,
     type: "coffee_shop",
@@ -818,7 +818,7 @@ export default [
   {
     id: 69,
     coins_per_hour: 2470,
-    respect: 1950,
+    respect: 10,
     level: 34,
     price: 490920,
     type: "coffee_shop",
@@ -830,7 +830,7 @@ export default [
   {
     id: 70,
     coins_per_hour: 2580,
-    respect: 2000,
+    respect: 10,
     level: 35,
     price: 540010,
     type: "coffee_shop",
@@ -842,9 +842,9 @@ export default [
   {
     id: 71,
     coins_per_hour: 400,
-    respect: 100,
+    respect: 10,
     level: 1,
-    price: 1000,
+    price: 3000,
     type: "zoo_shop",
     
     title: {
@@ -855,7 +855,7 @@ export default [
   {
     id: 72,
     coins_per_hour: 450,
-    respect: 200,
+    respect: 10,
     level: 2,
     price: 1500,
     type: "zoo_shop",
@@ -867,7 +867,7 @@ export default [
   {
     id: 73,
     coins_per_hour: 500,
-    respect: 300,
+    respect: 10,
     level: 3,
     price: 2250,
     type: "zoo_shop",
@@ -879,7 +879,7 @@ export default [
   {
     id: 74,
     coins_per_hour: 550,
-    respect: 400,
+    respect: 10,
     level: 4,
     price: 3380,
     type: "zoo_shop",
@@ -891,7 +891,7 @@ export default [
   {
     id: 75,
     coins_per_hour: 600,
-    respect: 500,
+    respect: 10,
     level: 5,
     price: 5070,
     type: "zoo_shop",
@@ -903,7 +903,7 @@ export default [
   {
     id: 76,
     coins_per_hour: 650,
-    respect: 550,
+    respect: 10,
     level: 6,
     price: 7610,
     type: "zoo_shop",
@@ -915,7 +915,7 @@ export default [
   {
     id: 77,
     coins_per_hour: 730,
-    respect: 600,
+    respect: 10,
     level: 7,
     price: 11420,
     type: "zoo_shop",
@@ -927,7 +927,7 @@ export default [
   {
     id: 78,
     coins_per_hour: 810,
-    respect: 650,
+    respect: 10,
     level: 8,
     price: 17130,
     type: "zoo_shop",
@@ -939,7 +939,7 @@ export default [
   {
     id: 79,
     coins_per_hour: 890,
-    respect: 700,
+    respect: 10,
     level: 9,
     price: 25700,
     type: "zoo_shop",
@@ -951,7 +951,7 @@ export default [
   {
     id: 80,
     coins_per_hour: 970,
-    respect: 750,
+    respect: 10,
     level: 10,
     price: 38550,
     type: "zoo_shop",
@@ -963,7 +963,7 @@ export default [
   {
     id: 81,
     coins_per_hour: 1070,
-    respect: 800,
+    respect: 10,
     level: 11,
     price: 57830,
     type: "zoo_shop",
@@ -975,7 +975,7 @@ export default [
   {
     id: 82,
     coins_per_hour: 1170,
-    respect: 850,
+    respect: 10,
     level: 12,
     price: 86750,
     type: "zoo_shop",
@@ -987,7 +987,7 @@ export default [
   {
     id: 83,
     coins_per_hour: 1270,
-    respect: 900,
+    respect: 10,
     level: 13,
     price: 130130,
     type: "zoo_shop",
@@ -999,7 +999,7 @@ export default [
   {
     id: 84,
     coins_per_hour: 1370,
-    respect: 950,
+    respect: 10,
     level: 14,
     price: 195200,
     type: "zoo_shop",
@@ -1011,7 +1011,7 @@ export default [
   {
     id: 85,
     coins_per_hour: 1470,
-    respect: 1000,
+    respect: 10,
     level: 15,
     price: 234240,
     type: "zoo_shop",
@@ -1023,7 +1023,7 @@ export default [
   {
     id: 86,
     coins_per_hour: 1570,
-    respect: 1050,
+    respect: 10,
     level: 16,
     price: 257660,
     type: "zoo_shop",
@@ -1035,7 +1035,7 @@ export default [
   {
     id: 87,
     coins_per_hour: 1670,
-    respect: 1100,
+    respect: 10,
     level: 17,
     price: 283430,
     type: "zoo_shop",
@@ -1047,7 +1047,7 @@ export default [
   {
     id: 88,
     coins_per_hour: 1770,
-    respect: 1150,
+    respect: 10,
     level: 18,
     price: 311770,
     type: "zoo_shop",
@@ -1059,7 +1059,7 @@ export default [
   {
     id: 89,
     coins_per_hour: 1870,
-    respect: 1200,
+    respect: 10,
     level: 19,
     price: 342950,
     type: "zoo_shop",
@@ -1071,7 +1071,7 @@ export default [
   {
     id: 90,
     coins_per_hour: 1970,
-    respect: 1250,
+    respect: 10,
     level: 20,
     price: 377250,
     type: "zoo_shop",
@@ -1083,7 +1083,7 @@ export default [
   {
     id: 91,
     coins_per_hour: 2090,
-    respect: 1300,
+    respect: 10,
     level: 21,
     price: 414980,
     type: "zoo_shop",
@@ -1095,7 +1095,7 @@ export default [
   {
     id: 92,
     coins_per_hour: 2210,
-    respect: 1350,
+    respect: 10,
     level: 22,
     price: 456480,
     type: "zoo_shop",
@@ -1107,7 +1107,7 @@ export default [
   {
     id: 93,
     coins_per_hour: 2330,
-    respect: 1400,
+    respect: 10,
     level: 23,
     price: 502130,
     type: "zoo_shop",
@@ -1119,7 +1119,7 @@ export default [
   {
     id: 94,
     coins_per_hour: 2450,
-    respect: 1450,
+    respect: 10,
     level: 24,
     price: 552340,
     type: "zoo_shop",
@@ -1131,7 +1131,7 @@ export default [
   {
     id: 95,
     coins_per_hour: 2600,
-    respect: 1500,
+    respect: 10,
     level: 25,
     price: 607570,
     type: "zoo_shop",
@@ -1143,7 +1143,7 @@ export default [
   {
     id: 96,
     coins_per_hour: 2750,
-    respect: 1550,
+    respect: 10,
     level: 26,
     price: 668330,
     type: "zoo_shop",
@@ -1155,7 +1155,7 @@ export default [
   {
     id: 97,
     coins_per_hour: 2900,
-    respect: 1600,
+    respect: 10,
     level: 27,
     price: 735160,
     type: "zoo_shop",
@@ -1167,7 +1167,7 @@ export default [
   {
     id: 98,
     coins_per_hour: 3050,
-    respect: 1650,
+    respect: 10,
     level: 28,
     price: 808680,
     type: "zoo_shop",
@@ -1179,7 +1179,7 @@ export default [
   {
     id: 99,
     coins_per_hour: 3200,
-    respect: 1700,
+    respect: 10,
     level: 29,
     price: 889550,
     type: "zoo_shop",
@@ -1191,7 +1191,7 @@ export default [
   {
     id: 100,
     coins_per_hour: 3350,
-    respect: 1750,
+    respect: 10,
     level: 30,
     price: 978510,
     type: "zoo_shop",
@@ -1203,7 +1203,7 @@ export default [
   {
     id: 101,
     coins_per_hour: 3550,
-    respect: 1800,
+    respect: 10,
     level: 31,
     price: 1076360,
     type: "zoo_shop",
@@ -1215,7 +1215,7 @@ export default [
   {
     id: 102,
     coins_per_hour: 3750,
-    respect: 1850,
+    respect: 10,
     level: 32,
     price: 1184000,
     type: "zoo_shop",
@@ -1227,7 +1227,7 @@ export default [
   {
     id: 103,
     coins_per_hour: 3950,
-    respect: 1900,
+    respect: 10,
     level: 33,
     price: 1302400,
     type: "zoo_shop",
@@ -1239,7 +1239,7 @@ export default [
   {
     id: 104,
     coins_per_hour: 4150,
-    respect: 1950,
+    respect: 10,
     level: 34,
     price: 1432640,
     type: "zoo_shop",
@@ -1251,7 +1251,7 @@ export default [
   {
     id: 105,
     coins_per_hour: 4350,
-    respect: 2000,
+    respect: 10,
     level: 35,
     price: 1575900,
     type: "zoo_shop",
