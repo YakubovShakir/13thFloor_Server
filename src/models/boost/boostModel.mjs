4d4eaa55d7c0c6109736833d656beac7a3125dfd
Leonid Brown <leonid.brown01@gmail.com>
2025-01-28 09:59:19 +0300

    fix: boosts migration

--- src/models/boost/boostModel.mjs
+++ src/models/boost/boostModel.mjs
@@ -3,13 +3,13 @@ import mongoose from "mongoose"
 const boostsSchema = new mongoose.Schema({
   boost_id: { type: Number, required: true, unique: true },
   name: {
-    ru: { type: String, unique: true },
-    en: { type: String, unique: true }
+    ru: { type: String },
+    en: { type: String }
   },
   stars_price: { type: Number, required: true },
   description: {
-    ru: { type: String, unique: true },
-    en: { type: String, unique: true }
+    ru: { type: String},
+    en: { type: String}
   },
   link: { type: String },
   duration: { type: Number },
