fa9de4c2aa52b320238e1396913d04a857f96b8c
Sayyeap <156927818+Sayyeap@users.noreply.github.com>
2025-02-12 01:06:01 +0300

    Фулллл ПУЛЛ склилов

--- src/models/skill/migration.js
+++ src/models/skill/migration.js
@@ -222,7 +222,7 @@ export default [
     
     name: {
       ru: "Слушать сплетни",
-      en: "Business Correspondence Course"
+      en: "Gossip Listening"
     },
     coins_price: 6300,
     requiredLevel: 6,
@@ -259,21 +259,6 @@ export default [
     skill_id: 14,
     
     name: {
-      ru: "Онлайн обучение",
-      en: "Online education"
-    },
-    coins_price: 12150,
-    requiredLevel: 7,
-    description: null,
-    duration: 180,
-    skill_id_required: 13,
-    type: null,
-    link: "https://13floorgame.ru/images/skills/onlineLearning.png",
-  },
-  {
-    skill_id: 15,
-    
-    name: {
       ru: "Консультация у карьерного коуча",
       en: "Consulting with a career coach"
     },
@@ -281,12 +266,61 @@ export default [
     requiredLevel: 7,
     description: null,
     duration: 180,
-    skill_id_required: 14,
+    skill_id_required: 13,
     type: null,
     link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%BA%D0%B0%D1%80%D1%8C%D0%B5%D1%80%D0%B0.png",
   },
   {
+    skill_id: 15,
+    
+    name: {
+      ru: "Онлайн обучение",
+      en: "Online education"
+    },
+    coins_price: 12150,
+    requiredLevel: 7,
+    description: null,
+    duration: 180,
+    skill_id_required: 14,
+    type: null,
+    link: "https://13floorgame.ru/images/skills/onlineLearning.png",
+  },
+  {
     skill_id: 16,
+    
+    name: {
+      ru: "Как продать ручку на собеседовании",
+      en: "How to sell a pen at an interview"
+    },
+    coins_price: 12150,
+    requiredLevel: 7,
+    description: null,
+    duration: 180,
+    skill_id_required: 15,
+    type: null,
+    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%BA%D0%B0%D0%BA-%D0%BF%D1%80%D0%BE%D0%B4%D0%B0%D1%82%D1%8C-%D1%80%D1%83%D1%87%D0%BA%D1%83.png",
+  },
+  {
+    skill_id: 17,
+    
+    name: {
+      ru: "Аналитик-экстрасенс",
+      en: "The psychic analyst"
+    },
+    coins_price: 12150,
+    requiredLevel: 7,
+    description: 
+    {
+      ru: "Угадай, сколько будет стоить картошка завтра. Подсказка: всё плохо",
+      en: "Guess how much a potato will cost tomorrow. Hint: everything is bad"
+    },
+    duration: 180,
+    skill_id_required: 16,
+    type: null,
+    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%AD%D0%BA%D1%81%D1%82%D1%80%D0%B0%D1%81%D0%B5%D0%BD%D1%81.png",
+  },
+  {
+    skill_id: 18,
     
     name: {
       ru: "Основы закупок",
@@ -294,14 +328,56 @@ export default [
     },
     coins_price: 19800,
     requiredLevel: 8,
-    description: null,
+    description: 
+    {
+      ru: "Уговори поставщика отдать тебе кофе за спасибо",
+      en: "Persuade the supplier to give you the coffee for a thank you"
+    },
     duration: 250,
-    skill_id_required: 8,
+    skill_id_required: 17,
     type: null,
     link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%B7%D0%B0%D0%BA%D1%83%D0%BF%D0%BA%D0%B8.png",
   },
   {
-    skill_id: 17,
+    skill_id: 19,
+    
+    name: {
+      ru: "Поставщик-призрак",
+      en: "The Ghost Supplier"
+    },
+    coins_price: 19800,
+    requiredLevel: 8,
+    description: 
+    {
+      ru: "Найди пропавшего рыбака. Он где-то между уже не работает и никого не волнует",
+      en: "Find the missing fisherman. It's somewhere between no longer working and nobody cares"
+    },
+    duration: 250,
+    skill_id_required: 18,
+    type: null,
+    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%BF%D0%BE%D1%81%D1%82%D0%B0%D0%B2%D1%89%D0%B8%D0%BA-%D0%BF%D1%80%D0%B8%D0%B7%D1%80%D0%B0%D0%BA.png",
+  },
+  {
+    skill_id: 20,
+    
+    name: {
+      ru: "Мастер заголовков",
+      en: "Text Header Wizard"
+    },
+    coins_price: 19800,
+    requiredLevel: 8,
+    description: 
+    {
+      ru: "Научись писать заголовки, от которых невозможно оторваться",
+      en: "Learn how to write headlines that are impossible to break away from"
+    },
+    duration: 250,
+    skill_id_required: 19,
+    type: null,
+    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%BE%D1%81%D0%BD%D0%BE%D0%B2%D0%B0-%D0%B7%D0%B0%D0%BA%D1%83%D0%BF%D0%BE%D0%BA.png",
+  },
+  {
+    skill_id: 21,
     
     name: {
       ru: "Тренинг по рекламе",
@@ -311,12 +387,31 @@ export default [
     requiredLevel: 9,
     description: null,
     duration: 400,
-    skill_id_required: 6,
+    skill_id_required: 20,
     type: null,
     link: "https://13floorgame.ru/images/skills/advertising.png",
   },
   {
-    skill_id: 18,
+    skill_id: 22,
+    
+    name: {
+      ru: "Креатив на минималках",
+      en: "Creative on minimums"
+    },
+    coins_price: 19800,
+    requiredLevel: 8,
+    description: 
+    {
+      ru: "Создай рекламу для чего-то скучного. Например, для серых носков. Да, это вызов",
+      en: "Create an ad for something boring. For example, for gray socks. Yes, it's a challenge"
+    },
+    duration: 250,
+    skill_id_required: 21,
+    type: null,
+    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%A1%D0%BC%D0%B5%D1%88%D0%BD%D0%BE%D0%B5-%D0%B2%D0%B8%D0%B4%D0%B5%D0%BE.png",
+  },
+  {
+    skill_id: 23,
     
     name: {
       ru: "Школа рекламы",
@@ -324,14 +419,56 @@ export default [
     },
     coins_price: 41000,
     requiredLevel: 10,
-    description: null,
+    description: 
+    {
+      ru: "Научись продавать даже то, что никто не хочет покупать. Например, снег зимой",
+      en: "Learn to sell even what no one wants to buy. For example, snow in winter"
+    },
     duration: 512,
-    skill_id_required: 10,
+    skill_id_required: 22,
     type: null,
     link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%BA%D0%B0%D1%80%D0%BA%D0%BA%D0%B5%D1%82%D0%B8%D0%BD%D0%B3.png",
   },
   {
-    skill_id: 19,
+    skill_id: 24,
+    
+    name: {
+      ru: "Аналитика без паники",
+      en: "Analytics without panic"
+    },
+    coins_price: 41000,
+    requiredLevel: 10,
+    description: 
+    {
+      ru: "Разберись в цифрах, которые все боятся. И сделай так, чтобы они работали на тебя",
+      en: "Figure out the numbers that everyone is afraid of. And make them work for you"
+    },
+    duration: 512,
+    skill_id_required: 23,
+    type: null,
+    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0.png",
+  },
+  {
+    skill_id: 25,
+    
+    name: {
+      ru: "Гуру соцсетей",
+      en: "The Social Media Guru"
+    },
+    coins_price: 41000,
+    requiredLevel: 10,
+    description: 
+    {
+      ru: "Создай пост, который взорвёт ленту. Но без котиков. Это сложный уровень",
+      en: "Create a post that will blow up the feed. But without the cats. This is a difficult level"
+    },
+    duration: 512,
+    skill_id_required: 24,
+    type: null,
+    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D1%82%D0%B5%D0%BB%D0%B5%D1%84%D0%BE%D0%BD-%D1%81%D0%BE%D1%86-%D1%81%D0%B5%D1%82%D0%B8.png",
+  },
+  {
+    skill_id: 26,
     
     name: {
       ru: "Основы брендинга",
@@ -339,14 +476,56 @@ export default [
     },
     coins_price: 54000,
     requiredLevel: 11,
-    description: null,
+    description: 
+    {
+      ru: "Создай бренд, который запомнят даже золотые рыбки",
+      en: "Create a brand that even goldfish will remember"
+    },
     duration: 760,
-    skill_id_required: 11,
+    skill_id_required: 25,
     type: null,
     link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%9E%D1%81%D0%BD%D0%BE%D0%B2%D1%8B-%D0%B1%D1%80%D0%B5%D0%BD%D0%B4%D0%B8%D0%BD%D0%B3%D0%B0.png",
   },
   {
-    skill_id: 20,
+    skill_id: 27,
+    
+    name: {
+      ru: "Легенда бренда",
+      en: "Brand legend"
+    },
+    coins_price: 54000,
+    requiredLevel: 11,
+    description: 
+    {
+      ru: "Придумай историю, которая заставит людей плакать. И покупать",
+      en: "Come up with a story that will make people cry. And buy"
+    },
+    duration: 760,
+    skill_id_required: 26,
+    type: null,
+    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%BB%D0%B5%D0%B3%D0%B5%D0%BD%D0%B4%D0%B0.png",
+  },
+  {
+    skill_id: 28,
+    
+    name: {
+      ru: "Кризисный менеджер",
+      en: "The Crisis Manager"
+    },
+    coins_price: 54000,
+    requiredLevel: 11,
+    description: 
+    {
+      ru: "Спаси бренд от провала. Или сделай вид, что так и было задумано",
+      en: "To save the brand from failure. Or pretend that it was meant to be"
+    },
+    duration: 760,
+    skill_id_required: 27,
+    type: null,
+    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%BA%D1%80%D0%B8%D0%B7%D0%B8%D1%81-%D0%BC%D0%B5%D0%BD%D0%B5%D0%B4%D0%B6%D0%B5%D1%80.png",
+  },
+  {
+    skill_id: 29,
     
     name: {
       ru: "Курсы подбора персонала",
@@ -354,35 +533,56 @@ export default [
     },
     coins_price: 71000,
     requiredLevel: 12,
-    description: null,
+    description: 
+    {
+      ru: "Научись находить идеальных сотрудников. Или хотя бы тех, кто не спит на собеседовании",
+      en: "Learn how to find the perfect employees. Or at least those who don't sleep during the interview"
+    },
     duration: 1100,
-    skill_id_required: 5,
+    skill_id_required: 28,
     type: null,
     link: "https://13floorgame.ru/images/skills/recrutCourses.png",
   },
-  // {
-  //   skill_id: 7,
-    
-  //   name: {
-  //     ru: "Статья о здоровом сне",
-  //     en: "An article about healthy sleep"
-  //   },
-
-  //   coins_price: 1000,
-  //   description: 
-  //   {
-  //     ru: "Твой сон длится меньше на 30 минут, но ты чувствуешь себя все так же бодро",
-  //     en: "Your sleep lasts less than 30 minutes, but you still feel cheerful"
-  //   },
-    
-  //   duration: 120,
-  //   requiredLevel: 1,
-  //   skill_id_required: null,
-  //   type: null,
-  //   link: "https://13floorgame.ru/images/skills/sleepMode.png",
-  // },
   {
-    skill_id: 21,
+    skill_id: 30,
+    
+    name: {
+      ru: "Мастер мотивации",
+      en: "The Master of Motivation"
+    },
+    coins_price: 71000,
+    requiredLevel: 12,
+    description: 
+    {
+      ru: "Вдохновляй людей работать за спасибо. Ну или хотя бы за кофе",
+      en: "Inspire people to work for thank you. Or at least for coffee"
+    },
+    duration: 1100,
+    skill_id_required: 29,
+    type: null,
+    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%BB%D0%B8%D0%B4%D0%B5%D1%80%D1%81%D1%82%D0%B2%D0%BE-%D0%B8-%D0%BC%D0%BE%D1%82%D0%B8%D0%B2%D0%B0%D1%86%D0%B8%D1%8F.png",
+  },
+  {
+    skill_id: 31,
+    
+    name: {
+      ru: "Мастер ресурсов",
+      en: "Chief of Resources"
+    },
+    coins_price: 71000,
+    requiredLevel: 12,
+    description: 
+    {
+      ru: "Добудь ресурсы из воздуха. Или из бюджета, если повезёт",
+      en: "Get resources out of thin air. Or from the budget, if you're lucky"
+    },
+    duration: 1100,
+    skill_id_required: 30,
+    type: null,
+    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%93%D1%80%D1%83%D0%B7%D1%87%D0%B8%D0%BA.png",
+  },
+  {
+    skill_id: 32,
     
     name: {
       ru: "Планирование и дедлайны",
@@ -390,14 +590,18 @@ export default [
     },
     coins_price: 91000,
     requiredLevel: 13,
-    description: null,
+    description: 
+    {
+      ru: "Научись ставить сроки так, чтобы всё успевали. Или хотя бы делали вид",
+      en: "Learn to set deadlines so that everyone can keep up. Or at least they pretended to"
+    },
     duration: 1700,
-    skill_id_required: 13,
+    skill_id_required: 31,
     type: null,
     link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%9A%D0%B0%D0%BB%D0%B5%D0%BD%D0%B4%D0%B0%D1%80%D1%8C.png",
   },
   {
-    skill_id: 22,
+    skill_id: 33,
     
     name: {
       ru: "Школа менеджмента",
@@ -405,14 +609,37 @@ export default [
     },
     coins_price: 129600,
     requiredLevel: 14,
-    description: null,
+    description: 
+    {
+      ru: "Научись вести команду так, чтобы никто не заметил, что ты сам не знаешь, что делаешь",
+      en: "Learn how to lead a team so that no one notices that you don't know what you're doing"
+    },
     duration: 2500,
-    skill_id_required: 14,
+    skill_id_required: 32,
     type: null,
     link: "https://13floorgame.ru/images/skills/management.png",
   },
   {
-    skill_id: 23,
+    skill_id: 34,
+    
+    name: {
+      ru: "Разрешение конфликтов",
+      en: "Conflict resolution"
+    },
+    coins_price: 129600,
+    requiredLevel: 14,
+    description: 
+    {
+      ru: "Стань мастером в гашении скандалов. Или просто раздай всем печеньки",
+      en: "Become a master at extinguishing scandals. Or just give out cookies to everyone"
+    },
+    duration: 2500,
+    skill_id_required: 33,
+    type: null,
+    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D1%80%D0%B0%D0%B7%D1%80%D0%B5%D1%88%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BA%D0%BE%D0%BD%D1%84%D0%BB%D0%B8%D0%BA%D1%82%D0%BE%D0%B2.png",
+  },
+  {
+    skill_id: 35,
     
     name: {
       ru: "Бизнес тренинг",
@@ -420,29 +647,75 @@ export default [
     },
     coins_price: 168000,
     requiredLevel: 15,
-    description: null,
+    description: 
+    {
+      ru: "Освой искусство принимать решения. Или просто кидай монетку",
+      en: "Master the art of making decisions. Or just flip a coin"
+    },
     duration: 3800,
-    skill_id_required: 15,
+    skill_id_required: 34,
     type: null,
     link: "https://13floorgame.ru/images/skills/businessTraining.png",
   },
   {
-    skill_id: 24,
+    skill_id: 36,
+    
+    name: {
+      ru: "Стратегия роста",
+      en: "Growth strategy"
+    },
+    coins_price: 168000,
+    requiredLevel: 15,
+    description: 
+    {
+      ru: "Придумай, как вырастить компанию. Или хотя бы как не уронить её",
+      en: "Figure out how to grow the company. Or at least how not to drop it"
+    },
+    duration: 3800,
+    skill_id_required: 35,
+    type: null,
+    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D1%81%D1%82%D1%80%D0%B5%D1%82%D0%B5%D0%B3%D0%B8%D1%8F-%D1%80%D0%BE%D1%81%D1%82%D0%B0.png",
+  },
+  {
+    skill_id: 37,
    
     name: {
       ru: "Лидерство и мотивация",
       en: "Leadership and motivation"
     },
     coins_price: 216000,
-    requiredLevel: 16,
-    description: null,
+    requiredLevel: 15,
+    description: 
+    {
+      ru: "Веди за собой так, чтобы никто не заметил, что ты сам не знаешь куда",
+      en: "Lead the way so that no one notices that you don't know where you're going"
+    },
     duration: 5800,
-    skill_id_required: 16,
+    skill_id_required: 36,
     type: null,
     link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%9C%D0%BE%D1%82%D0%B8%D0%B2%D0%B0%D1%86%D0%B8%D1%8F-%D0%B8-%D1%83%D1%81%D0%BF%D0%B5%D1%85.png",
   },
   {
-    skill_id: 25,
+    skill_id: 38,
+   
+    name: {
+      ru: "Глобальное видение",
+      en: "Global vision"
+    },
+    coins_price: 216000,
+    requiredLevel: 15,
+    description: 
+    {
+      ru: "Придумай, куда двигаться компании. Или просто скажи вперёд",
+      en: "Figure out where to move the company. Or just say go ahead"
+    },
+    duration: 5800,
+    skill_id_required: 37,
+    type: null,
+    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%93%D0%BB%D0%BE%D0%B1%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D0%B5-%D0%B2%D0%B8%D0%B4%D0%B5%D0%BD%D1%8C%D0%B5.png",
+  },
+  {
+    skill_id: 39,
    
     name: {
       ru: "Тренинг по автоматизации",
@@ -450,14 +723,94 @@ export default [
     },
     coins_price: 273000,
     requiredLevel: 17,
-    description: null,
+    description: 
+    {
+      ru: "Научись заменять людей роботами. Или хотя бы Excel-таблицами",
+      en: "Learn how to replace people with robots. Or at least with Excel spreadsheets"
+    },
     duration: 8700,
-    skill_id_required: 17,
+    skill_id_required: 38,
     type: null,
     link: "https://13floorgame.ru/images/skills/automationTraining.png",
   },
   {
-    skill_id: 26,
+    skill_id: 40,
+   
+    name: {
+      ru: "Кризисное управление",
+      en: "Crisis management"
+    },
+    coins_price: 273000,
+    requiredLevel: 17,
+    description: 
+    {
+      ru: "Спасай компанию от краха. Или просто сделай вид, что всё идёт по плану",
+      en: "Save the company from collapse. Or just pretend that everything is going according to plan"
+    },
+    duration: 8700,
+    skill_id_required: 39,
+    type: null,
+    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%BF%D1%80%D0%BE%D1%87%D0%B5%D1%81%D1%82%D1%8C-%D1%81%D1%82%D0%B0%D1%82%D1%8C%D1%8E.png",
+  },
+  {
+    skill_id: 41,
+   
+    name: {
+      ru: "Стратегическое мышление",
+      en: "Strategic thinking"
+    },
+    coins_price: 273000,
+    requiredLevel: 17,
+    description: 
+    {
+      ru: "Придумай, как захватить рынок. Или хотя бы как удержаться на плаву",
+      en: "Figure out how to capture the market. Or at least how to stay afloat"
+    },
+    duration: 8700,
+    skill_id_required: 40,
+    type: null,
+    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D1%81%D1%82%D1%80%D0%B0%D1%82%D0%B5%D0%B3%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%BE%D0%B5-%D0%BC%D1%8B%D1%88%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5.png",
+  },
+  {
+    skill_id: 42,
+ 
+    name: {
+      ru: "Экономическое образование",
+      en: "Economic education"
+    },
+    coins_price: 660000,
+    requiredLevel: 20,
+    description: 
+    {
+      ru: "Пойми, куда уходят деньги. Или просто найди того, кто знает",
+      en: "Understand where the money goes. Or just find someone who knows"
+    },
+    duration: 13800,
+    skill_id_required: 41,
+    type: null,
+    link: "https://13floorgame.ru/images/skills/economicEducation.png",
+  },
+  {
+    skill_id: 43,
+ 
+    name: {
+      ru: "Искусство делегирования",
+      en: "The Art of delegation"
+    },
+    coins_price: 660000,
+    requiredLevel: 20,
+    description: 
+    {
+      ru: "Найди людей, которые будут работать за тебя. Или хотя бы делают вид",
+      en: "Find people who will work for you. Or at least pretend to"
+    },
+    duration: 13800,
+    skill_id_required: 41,
+    type: null,
+    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%B8%D1%81%D1%81%D0%BA%D1%83%D1%81%D1%82%D0%B2%D0%BE-%D0%B4%D0%B5%D0%BB%D0%B8%D0%B3%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C.png",
+  },
+  {
+    skill_id: 44,
     
     name: {
       ru: "Энциклопедия бодибилдинга",
@@ -475,20 +828,5 @@ export default [
     skill_id_required: null,
     type: null,
     link: "https://13floorgame.ru/images/skills/bodybuilding.png",
-  },
-  {
-    skill_id: 27,
- 
-    name: {
-      ru: "Экономическое образование",
-      en: "Economic education"
-    },
-    coins_price: 660000,
-    requiredLevel: 20,
-    description: null,
-    duration: 13800,
-    skill_id_required: 19,
-    type: null,
-    link: "https://13floorgame.ru/images/skills/economicEducation.png",
   },
 ]
