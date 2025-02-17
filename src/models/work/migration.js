0e692f37f450cab0d1397d65e773b50d73d29711
Leonid Brown <leonid.brown01@gmail.com>
2025-02-17 15:33:15 +0700

    fix: work migration recalculated

--- src/models/work/migration.js
+++ src/models/work/migration.js
@@ -1,362 +1,362 @@
 export default [
   {
-    work_id: 1,
-    name: {
-      ru: "Уборщик",
-      en: "Janitor"
+    "work_id": 1,
+    "name": {
+      "ru": "Уборщик",
+      "en": "Janitor"
     },
-    requiredLevel: 1,
-    coins_price: 1,
-    coins_per_minute: 900,
-    energy_cost_per_minute: 100,
-    mood_cost_per_minute: 20,
-    hungry_cost_per_minute: 28,
-    skill_id_required: null,
-    respect_required: 0,
-    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%A3%D0%B1%D0%BE%D1%80%D1%89%D0%B8%D0%BA.png",
-    duration: 1,
-    experience_reward: 15
+    "requiredLevel": 1,
+    "coins_price": 1,
+    "coins_in_hour": 15,
+    "energy_cost_per_minute": 1.6666666666666667,
+    "mood_cost_per_minute": 0.3333333333333333,
+    "hungry_cost_per_minute": 0.4666666666666667,
+    "skill_id_required": null,
+    "respect_required": 0,
+    "link": "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%A3%D0%B1%D0%BE%D1%80%D1%89%D0%B8%D0%BA.png",
+    "duration": 1,
+    "experience_reward": 15
   },
   {
-    work_id: 2,
-    name: {
-      ru: "Курьер",
-      en: "Courier"
+    "work_id": 2,
+    "name": {
+      "ru": "Курьер",
+      "en": "Courier"
     },
-    requiredLevel: 2,
-    coins_price: 23,
-    coins_per_minute: 1200,
-    energy_cost_per_minute: 150,
-    mood_cost_per_minute: 22,
-    hungry_cost_per_minute: 30,
-    skill_id_required: 1,
-    respect_required: 50,
-    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%BA%D1%83%D1%80%D1%8C%D0%B5%D1%80.png",
-    duration: 5,
-    experience_reward: 100
+    "requiredLevel": 2,
+    "coins_price": 23,
+    "coins_in_hour": 20,
+    "energy_cost_per_minute": 2.5,
+    "mood_cost_per_minute": 0.3666666666666667,
+    "hungry_cost_per_minute": 0.5,
+    "skill_id_required": 1,
+    "respect_required": 50,
+    "link": "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%BA%D1%83%D1%80%D1%8C%D0%B5%D1%80.png",
+    "duration": 5,
+    "experience_reward": 100
   },
   {
-    work_id: 3,
-    name: {
-      ru: "Мойщик тарелок",
-      en: "Plate washer"
+    "work_id": 3,
+    "name": {
+      "ru": "Мойщик тарелок",
+      "en": "Plate washer"
     },
-    requiredLevel: 3,
-    coins_price: 60,
-    coins_per_minute: 1500,
-    energy_cost_per_minute: 200,
-    mood_cost_per_minute: 24,
-    hungry_cost_per_minute: 32,
-    skill_id_required: 3,
-    respect_required: 150,
-    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%BC%D0%BE%D0%B9%D1%89%D0%B8%D0%BA-%D1%82%D0%B0%D1%80%D0%B5%D0%BB%D0%BE%D0%BA.png",
-    duration: 8,
-    experience_reward: 200
+    "requiredLevel": 3,
+    "coins_price": 60,
+    "coins_in_hour": 25,
+    "energy_cost_per_minute": 3.3333333333333335,
+    "mood_cost_per_minute": 0.4,
+    "hungry_cost_per_minute": 0.5333333333333333,
+    "skill_id_required": 3,
+    "respect_required": 150,
+    "link": "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%BC%D0%BE%D0%B9%D1%89%D0%B8%D0%BA-%D1%82%D0%B0%D1%80%D0%B5%D0%BB%D0%BE%D0%BA.png",
+    "duration": 8,
+    "experience_reward": 200
   },
   {
-    work_id: 4,
-    name: {
-      ru: "Официант",
-      en: "Waiter"
+    "work_id": 4,
+    "name": {
+      "ru": "Официант",
+      "en": "Waiter"
     },
-    requiredLevel: 4,
-    coins_price: 250,
-    coins_per_minute: 1800,
-    energy_cost_per_minute: 300,
-    mood_cost_per_minute: 26,
-    hungry_cost_per_minute: 34,
-    skill_id_required: 5,
-    respect_required: 300,
-    link: "https://13floorgame.ru/images/works/waiter.png",
-    duration: 12,
-    experience_reward: 360
+    "requiredLevel": 4,
+    "coins_price": 250,
+    "coins_in_hour": 30,
+    "energy_cost_per_minute": 5,
+    "mood_cost_per_minute": 0.43333333333333335,
+    "hungry_cost_per_minute": 0.5666666666666667,
+    "skill_id_required": 5,
+    "respect_required": 300,
+    "link": "https://13floorgame.ru/images/works/waiter.png",
+    "duration": 12,
+    "experience_reward": 360
   },
   {
-    work_id: 5,
-    name: {
-      ru: "Администратор",
-      en: "Manager"
+    "work_id": 5,
+    "name": {
+      "ru": "Администратор",
+      "en": "Manager"
     },
-    requiredLevel: 5,
-    coins_price: 2000,
-    coins_per_minute: 1920,
-    energy_cost_per_minute: 500,
-    mood_cost_per_minute: 28,
-    hungry_cost_per_minute: 36,
-    skill_id_required: 8,
-    respect_required: 600,
-    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%9C%D0%B5%D0%BD%D0%B5%D0%B4%D0%B6%D0%B5%D1%80-%D1%80%D0%B5%D1%81%D1%82%D0%B8%D0%BA%D0%B0.png",
-    duration: 20,
-    experience_reward: 640
+    "requiredLevel": 5,
+    "coins_price": 2000,
+    "coins_in_hour": 32,
+    "energy_cost_per_minute": 8.333333333333334,
+    "mood_cost_per_minute": 0.4666666666666667,
+    "hungry_cost_per_minute": 0.6,
+    "skill_id_required": 8,
+    "respect_required": 600,
+    "link": "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%9C%D0%B5%D0%BD%D0%B5%D0%B4%D0%B6%D0%B5%D1%80-%D1%80%D0%B5%D1%81%D1%82%D0%B8%D0%BA%D0%B0.png",
+    "duration": 20,
+    "experience_reward": 640
   },
   {
-    work_id: 6,
-    name: {
-      ru: "Секретарь",
-      en: "Secretary"
+    "work_id": 6,
+    "name": {
+      "ru": "Секретарь",
+      "en": "Secretary"
     },
-    requiredLevel: 6,
-    coins_price: 5760,
-    coins_per_minute: 2280,
-    energy_cost_per_minute: 600,
-    mood_cost_per_minute: 30,
-    hungry_cost_per_minute: 38,
-    skill_id_required: 11,
-    respect_required: 800,
-    link: "https://13floorgame.ru/images/works/officeManager.png",
-    duration: 30,
-    experience_reward: 1140
+    "requiredLevel": 6,
+    "coins_price": 5760,
+    "coins_in_hour": 38,
+    "energy_cost_per_minute": 10,
+    "mood_cost_per_minute": 0.5,
+    "hungry_cost_per_minute": 0.6333333333333333,
+    "skill_id_required": 11,
+    "respect_required": 800,
+    "link": "https://13floorgame.ru/images/works/officeManager.png",
+    "duration": 30,
+    "experience_reward": 1140
   },
   {
-    work_id: 7,
-    name: {
-      ru: "Офис-менеджер",
-      en: "Office Manager"
+    "work_id": 7,
+    "name": {
+      "ru": "Офис-менеджер",
+      "en": "Office Manager"
     },
-    requiredLevel: 7,
-    coins_price: 7980,
-    coins_per_minute: 2520,
-    energy_cost_per_minute: 650,
-    mood_cost_per_minute: 34,
-    hungry_cost_per_minute: 40,
-    skill_id_required: 13,
-    respect_required: 1200,
-    link: "https://13floorgame.ru/images/works/admin.png",
-    duration: 45,
-    experience_reward: 1890
+    "requiredLevel": 7,
+    "coins_price": 7980,
+    "coins_in_hour": 42,
+    "energy_cost_per_minute": 10.833333333333334,
+    "mood_cost_per_minute": 0.5666666666666667,
+    "hungry_cost_per_minute": 0.6666666666666666,
+    "skill_id_required": 13,
+    "respect_required": 1200,
+    "link": "https://13floorgame.ru/images/works/admin.png",
+    "duration": 45,
+    "experience_reward": 1890
   },
   {
-    work_id: 8,
-    name: {
-      ru: "Менеджер по продажам",
-      en: "Sales Manager"
+    "work_id": 8,
+    "name": {
+      "ru": "Менеджер по продажам",
+      "en": "Sales Manager"
     },
-    requiredLevel: 8,
-    coins_price: 11340,
-    coins_per_minute: 2760,
-    energy_cost_per_minute: 900,
-    mood_cost_per_minute: 38,
-    hungry_cost_per_minute: 44,
-    skill_id_required: 16,
-    respect_required: 1400,
-    link: "https://13floorgame.ru/images/works/salesManager.png",
-    duration: 50,
-    experience_reward: 2300
+    "requiredLevel": 8,
+    "coins_price": 11340,
+    "coins_in_hour": 46,
+    "energy_cost_per_minute": 15,
+    "mood_cost_per_minute": 0.6333333333333333,
+    "hungry_cost_per_minute": 0.7333333333333333,
+    "skill_id_required": 16,
+    "respect_required": 1400,
+    "link": "https://13floorgame.ru/images/works/salesManager.png",
+    "duration": 50,
+    "experience_reward": 2300
   },
   {
-    work_id: 9,
-    name: {
-      ru: "Менеджер по закупкам",
-      en: "Purchasing Manager"
+    "work_id": 9,
+    "name": {
+      "ru": "Менеджер по закупкам",
+      "en": "Purchasing Manager"
     },
-    requiredLevel: 9,
-    coins_price: 20700,
-    coins_per_minute: 3300,
-    energy_cost_per_minute: 1000,
-    mood_cost_per_minute: 42,
-    hungry_cost_per_minute: 48,
-    skill_id_required: 19,
-    respect_required: 2500,
-    link: "https://13floorgame.ru/images/works/purchasingManager.png",
-    duration: 55,
-    experience_reward: 3025
+    "requiredLevel": 9,
+    "coins_price": 20700,
+    "coins_in_hour": 55,
+    "energy_cost_per_minute": 16.666666666666668,
+    "mood_cost_per_minute": 0.7,
+    "hungry_cost_per_minute": 0.8,
+    "skill_id_required": 19,
+    "respect_required": 2500,
+    "link": "https://13floorgame.ru/images/works/purchasingManager.png",
+    "duration": 55,
+    "experience_reward": 3025
   },
   {
-    work_id: 10,
-    name: {
-      ru: "Копирайтер",
-      en: "Copywriter"
+    "work_id": 10,
+    "name": {
+      "ru": "Копирайтер",
+      "en": "Copywriter"
     },
-    requiredLevel: 10,
-    coins_price: 26400,
-    coins_per_minute: 3600,
-    energy_cost_per_minute: 1100,
-    mood_cost_per_minute: 46,
-    hungry_cost_per_minute: 52,
-    skill_id_required: 22,
-    respect_required: 2700,
-    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%9A%D0%BE%D0%BF%D0%B8%D1%80%D0%B0%D0%B9%D1%82%D0%B5%D1%80.png",
-    duration: 60,
-    experience_reward: 3600
+    "requiredLevel": 10,
+    "coins_price": 26400,
+    "coins_in_hour": 60,
+    "energy_cost_per_minute": 18.333333333333332,
+    "mood_cost_per_minute": 0.7666666666666667,
+    "hungry_cost_per_minute": 0.8666666666666667,
+    "skill_id_required": 22,
+    "respect_required": 2700,
+    "link": "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%9A%D0%BE%D0%BF%D0%B8%D1%80%D0%B0%D0%B9%D1%82%D0%B5%D1%80.png",
+    "duration": 60,
+    "experience_reward": 3600
   },
   {
-    work_id: 11,
-    name: {
-      ru: "Маркетолог",
-      en: "Marketer"
+    "work_id": 11,
+    "name": {
+      "ru": "Маркетолог",
+      "en": "Marketer"
     },
-    requiredLevel: 11,
-    coins_price: 41400,
-    coins_per_minute: 4080,
-    energy_cost_per_minute: 1200,
-    mood_cost_per_minute: 50,
-    hungry_cost_per_minute: 56,
-    skill_id_required: 25,
-    respect_required: 3000,
-    link: "https://13floorgame.ru/images/works/marketer.png",
-    duration: 70,
-    experience_reward: 4760
+    "requiredLevel": 11,
+    "coins_price": 41400,
+    "coins_in_hour": 68,
+    "energy_cost_per_minute": 20,
+    "mood_cost_per_minute": 0.8333333333333334,
+    "hungry_cost_per_minute": 0.9333333333333333,
+    "skill_id_required": 25,
+    "respect_required": 3000,
+    "link": "https://13floorgame.ru/images/works/marketer.png",
+    "duration": 70,
+    "experience_reward": 4760
   },
   {
-    work_id: 12,
-    name: {
-      ru: "Бренд-менеджер",
-      en: "Brand Manager"
+    "work_id": 12,
+    "name": {
+      "ru": "Бренд-менеджер",
+      "en": "Brand Manager"
     },
-    requiredLevel: 12,
-    coins_price: 44880,
-    coins_per_minute: 4500,
-    energy_cost_per_minute: 1300,
-    mood_cost_per_minute: 54,
-    hungry_cost_per_minute: 60,
-    skill_id_required: 28,
-    respect_required: 4000,
-    link: "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%B1%D1%80%D0%B5%D0%BD%D0%B4-%D0%BC%D0%B5%D0%BD%D0%B5%D0%B4%D0%B6%D0%B5%D1%80.png",
-    duration: 75,
-    experience_reward: 5625
+    "requiredLevel": 12,
+    "coins_price": 44880,
+    "coins_in_hour": 75,
+    "energy_cost_per_minute": 21.666666666666668,
+    "mood_cost_per_minute": 0.9,
+    "hungry_cost_per_minute": 1,
+    "skill_id_required": 28,
+    "respect_required": 4000,
+    "link": "https://d8bddedf-ac40-4488-8101-05035bb63d25.selstorage.ru/icons%2F%D0%B1%D1%80%D0%B5%D0%BD%D0%B4-%D0%BC%D0%B5%D0%BD%D0%B5%D0%B4%D0%B6%D0%B5%D1%80.png",
+    "duration": 75,
+    "experience_reward": 5625
   },
   {
-    work_id: 13,
-    name: {
-      ru: "HR",
-      en: "HR"
+    "work_id": 13,
+    "name": {
+      "ru": "HR",
+      "en": "HR"
     },
-    requiredLevel: 13,
-    coins_price: 56250,
-    coins_per_minute: 5100,
-    energy_cost_per_minute: 1400,
-    mood_cost_per_minute: 58,
-    hungry_cost_per_minute: 64,
-    skill_id_required: 30,
-    respect_required: 5000,
-    link: "https://13floorgame.ru/images/works/hr.png",
-    duration: 85,
-    experience_reward: 7225
+    "requiredLevel": 13,
+    "coins_price": 56250,
+    "coins_in_hour": 85,
+    "energy_cost_per_minute": 23.333333333333332,
+    "mood_cost_per_minute": 0.9666666666666667,
+    "hungry_cost_per_minute": 1.0666666666666667,
+    "skill_id_required": 30,
+    "respect_required": 5000,
+    "link": "https://13floorgame.ru/images/works/hr.png",
+    "duration": 85,
+    "experience_reward": 7225
   },
   {
-    work_id: 14,
-    name: {
-      ru: "Менеджер проекта",
-      en: "Project Manager"
+    "work_id": 14,
+    "name": {
+      "ru": "Менеджер проекта",
+      "en": "Project Manager"
     },
-    requiredLevel: 14,
-    coins_price: 102000,
-    coins_per_minute: 5700,
-    energy_cost_per_minute: 1500,
-    mood_cost_per_minute: 62,
-    hungry_cost_per_minute: 68,
-    skill_id_required: 32,
-    respect_required: 7500,
-    link: "https://13floorgame.ru/images/works/productManager.png",
-    duration: 95,
-    experience_reward: 9025
+    "requiredLevel": 14,
+    "coins_price": 102000,
+    "coins_in_hour": 95,
+    "energy_cost_per_minute": 25,
+    "mood_cost_per_minute": 1.0333333333333334,
+    "hungry_cost_per_minute": 1.1333333333333333,
+    "skill_id_required": 32,
+    "respect_required": 7500,
+    "link": "https://13floorgame.ru/images/works/productManager.png",
+    "duration": 95,
+    "experience_reward": 9025
   },
   {
-    work_id: 15,
-    name: {
-      ru: "Тимлид",
-      en: "Team Lead"
+    "work_id": 15,
+    "name": {
+      "ru": "Тимлид",
+      "en": "Team Lead"
     },
-    requiredLevel: 15,
-    coins_price: 128250,
-    coins_per_minute: 7200,
-    energy_cost_per_minute: 1600,
-    mood_cost_per_minute: 66,
-    hungry_cost_per_minute: 72,
-    skill_id_required: 34,
-    respect_required: 9000,
-    link: "https://13floorgame.ru/images/works/teamLid.png",
-    duration: 110,
-    experience_reward: 13200
+    "requiredLevel": 15,
+    "coins_price": 128250,
+    "coins_in_hour": 120,
+    "energy_cost_per_minute": 26.666666666666668,
+    "mood_cost_per_minute": 1.1,
+    "hungry_cost_per_minute": 1.2,
+    "skill_id_required": 34,
+    "respect_required": 9000,
+    "link": "https://13floorgame.ru/images/works/teamLid.png",
+    "duration": 110,
+    "experience_reward": 13200
   },
   {
-    work_id: 16,
-    name: {
-      ru: "Зам директора",
-      en: "Deputy Director"
+    "work_id": 16,
+    "name": {
+      "ru": "Зам директора",
+      "en": "Deputy Director"
     },
-    requiredLevel: 16,
-    coins_price: 216000,
-    coins_per_minute: 8400,
-    energy_cost_per_minute: 1700,
-    mood_cost_per_minute: 70,
-    hungry_cost_per_minute: 76,
-    skill_id_required: 36,
-    respect_required: 12000,
-    link: "https://13floorgame.ru/images/works/deputyDirector.png",
-    duration: 125,
-    experience_reward: 17500
+    "requiredLevel": 16,
+    "coins_price": 216000,
+    "coins_in_hour": 140,
+    "energy_cost_per_minute": 28.333333333333332,
+    "mood_cost_per_minute": 1.1666666666666667,
+    "hungry_cost_per_minute": 1.2666666666666666,
+    "skill_id_required": 36,
+    "respect_required": 12000,
+    "link": "https://13floorgame.ru/images/works/deputyDirector.png",
+    "duration": 125,
+    "experience_reward": 17500
   },
   {
-    work_id: 17,
-    name: {
-      ru: "Директор",
-      en: "Executive"
+    "work_id": 17,
+    "name": {
+      "ru": "Директор",
+      "en": "Executive"
     },
-    requiredLevel: 17,
-    coins_price: 273000,
-    coins_per_minute: 9600,
-    energy_cost_per_minute: 1800,
-    mood_cost_per_minute: 74,
-    hungry_cost_per_minute: 80,
-    skill_id_required: 38,
-    respect_required: 15000,
-    link: "https://13floorgame.ru/images/works/director.png",
-    duration: 140,
-    experience_reward: 22400
+    "requiredLevel": 17,
+    "coins_price": 273000,
+    "coins_in_hour": 160,
+    "energy_cost_per_minute": 30,
+    "mood_cost_per_minute": 1.2333333333333334,
+    "hungry_cost_per_minute": 1.3333333333333333,
+    "skill_id_required": 38,
+    "respect_required": 15000,
+    "link": "https://13floorgame.ru/images/works/director.png",
+    "duration": 140,
+    "experience_reward": 22400
   },
   {
-    work_id: 18,
-    name: {
-      ru: "Партнер",
-      en: "Partner"
+    "work_id": 18,
+    "name": {
+      "ru": "Партнер",
+      "en": "Partner"
     },
-    requiredLevel: 18,
-    coins_price: 360000,
-    coins_per_minute: 10800,
-    energy_cost_per_minute: 1900,
-    mood_cost_per_minute: 78,
-    hungry_cost_per_minute: 84,
-    skill_id_required: 40,
-    respect_required: 18000,
-    link: "https://13floorgame.ru/images/works/partner.png",
-    duration: 155,
-    experience_reward: 27900
+    "requiredLevel": 18,
+    "coins_price": 360000,
+    "coins_in_hour": 180,
+    "energy_cost_per_minute": 31.666666666666668,
+    "mood_cost_per_minute": 1.3,
+    "hungry_cost_per_minute": 1.4,
+    "skill_id_required": 40,
+    "respect_required": 18000,
+    "link": "https://13floorgame.ru/images/works/partner.png",
+    "duration": 155,
+    "experience_reward": 27900
   },
   {
-    work_id: 19,
-    name: {
-      ru: "CEO",
-      en: "CEO"
+    "work_id": 19,
+    "name": {
+      "ru": "CEO",
+      "en": "CEO"
     },
-    requiredLevel: 19,
-    coins_price: 486000,
-    coins_per_minute: 12000,
-    energy_cost_per_minute: 2000,
-    mood_cost_per_minute: 82,
-    hungry_cost_per_minute: 88,
-    skill_id_required: 42,
-    respect_required: 19000,
-    link: "https://13floorgame.ru/images/works/seo.png",
-    duration: 170,
-    experience_reward: 34000
+    "requiredLevel": 19,
+    "coins_price": 486000,
+    "coins_in_hour": 200,
+    "energy_cost_per_minute": 33.333333333333336,
+    "mood_cost_per_minute": 1.3666666666666667,
+    "hungry_cost_per_minute": 1.4666666666666666,
+    "skill_id_required": 42,
+    "respect_required": 19000,
+    "link": "https://13floorgame.ru/images/works/seo.png",
+    "duration": 170,
+    "experience_reward": 34000
   },
   {
-    work_id: 20,
-    name: {
-      ru: "Миллионер",
-      en: "Millionaire"
+    "work_id": 20,
+    "name": {
+      "ru": "Миллионер",
+      "en": "Millionaire"
     },
-    requiredLevel: 20,
-    coins_price: 660000,
-    coins_per_minute: 15000,
-    energy_cost_per_minute: 3000,
-    mood_cost_per_minute: 86,
-    hungry_cost_per_minute: 92,
-    skill_id_required: 43,
-    respect_required: 20000,
-    link: "https://13floorgame.ru/images/works/millionaire.png",
-    duration: 185,
-    experience_reward: 46250
-  },
+    "requiredLevel": 20,
+    "coins_price": 660000,
+    "coins_in_hour": 250,
+    "energy_cost_per_minute": 50,
+    "mood_cost_per_minute": 1.4333333333333333,
+    "hungry_cost_per_minute": 1.5333333333333334,
+    "skill_id_required": 43,
+    "respect_required": 20000,
+    "link": "https://13floorgame.ru/images/works/millionaire.png",
+    "duration": 185,
+    "experience_reward": 46250
+  }
 ]
