7f9e47362ec821a0d603d9c56b5ea2fe20832450
Sayyeap <156927818+Sayyeap@users.noreply.github.com>
2025-03-21 11:55:58 +0300

    описания

--- src/models/food/migration.js
+++ src/models/food/migration.js
@@ -5,6 +5,10 @@ export default [
       ru: "Яблоко",
       en: "Apple"},    
     duration: 2,
+    description: {
+      ru: 'Хрустящий намёк, что ты сегодня за здоровый образ жизни',
+      en: 'A crunchy hint that you’re all about that healthy life today'
+    },
     instant_energy_restore: null,
     long_energy_restore: null,
     instant_hungry_restore: {
@@ -24,6 +28,10 @@ export default [
       ru: "Печенька",
       en: "Cookie"},    
     duration: 5,
+    description: {
+      ru: 'Маленький кусочек счастья, который тает во рту и на совести',
+      en: 'A tiny chunk of joy that melts in your mouth and on your conscience'
+    },
     instant_energy_restore: null,
     long_energy_restore: null,
     instant_hungry_restore: {
@@ -48,6 +56,10 @@ export default [
       ru: "Полезный Салат",
       en: "Healthy Salad"},
     duration: 10,
+    description: {
+      ru: 'Зелёная месть калориям с заправкой из самодовольства',
+      en: 'A green revenge on calories with a dressing of smugness'
+    },
     instant_energy_restore: null,
     long_energy_restore: null,
     instant_hungry_restore: {
@@ -69,6 +81,10 @@ export default [
       ru: "Завтрак",
       en: "Breakfast"},
     duration: 15,
+    description: {
+      ru: 'Утренний подкуп для организма, чтобы встать с кровати',
+      en: 'A morning bribe for your body to get out of bed'
+    },
     instant_energy_restore: null,
     long_energy_restore: null,
     instant_hungry_restore: {
@@ -89,6 +105,10 @@ export default [
       ru: "Рамен",
       en: "Ramen"},
     duration: 30,
+    description: {
+      ru: 'Горячая лапша, которая обнимает душу через желудок',
+      en: 'Hot noodles that hug your soul via your stomach'
+    },
     instant_energy_restore: null,
     long_energy_restore: null,
     instant_hungry_restore: {
@@ -112,6 +132,10 @@ export default [
       ru: "Комплексный обед",
       en: "Lunch"},
     duration: 30,
+    description: {
+      ru: 'Набор еды, чтобы выжить до ужина без вопросов к шефу',
+      en: 'A food kit to survive till dinner without bugging the boss'
+    },
     instant_energy_restore: null,
     long_energy_restore: null,
     instant_hungry_restore: {
@@ -135,6 +159,10 @@ export default [
       ru: "Пицца",
       en: "Pizza"},
     duration: 40,
+    description: {
+      ru: 'Круглый праздник для желудка, где сыр — главный гость',
+      en: 'A round party for your stomach, where cheese is the VIP'
+    },
     instant_energy_restore: null,
     long_energy_restore: null,
     instant_hungry_restore: {
@@ -159,6 +187,10 @@ export default [
       ru: "Протеиновый батончик",
       en: "Protein Bar"},
     duration: 45,
+    description: {
+      ru: 'Сладкая ложь для мышц, что ты теперь спортсмен',
+      en: 'Sweet lie to your muscles that you’re an athlete now'
+    },
     instant_energy_restore: null,
     long_energy_restore: null,
     instant_hungry_restore: {
@@ -182,6 +214,10 @@ export default [
       ru: "Сытный бургер",
       en: "Big burger"},
     duration: 60,
+    description: {
+      ru: 'Булка с мясом обещает, что ты не встанешь из-за стола',
+      en: 'А bun with meat promises you won’t get up from the table'
+    },
     instant_energy_restore: null,
     long_energy_restore: null,
     instant_hungry_restore: {
@@ -205,6 +241,10 @@ export default [
       ru: "Стейк",
       en: "Steak"},
     duration: 220,
+    description: {
+      ru: 'Это когда мясо притворяется, что оно всё ещё живо, но уже вкусно',
+      en: 'When meat pretends it’s still alive, but it’s already delicious'
+    },
     instant_energy_restore: null,
     long_energy_restore: null,
     instant_hungry_restore: {
@@ -225,6 +265,10 @@ export default [
       ru: "Суши",
       en: "Sushi"},
     duration: 140,
+    description: {
+      ru: 'Лучший способ улучшить настроение ',
+      en: 'The best way to improve your mood'
+    },
     instant_energy_restore: null,
     long_energy_restore: null,
     instant_hungry_restore: {
@@ -248,6 +292,10 @@ export default [
       ru: "Буррито",
       en: "Burrito"},
     duration: 45,
+    description: {
+      ru: 'Большой и сытный',
+      en: 'A large and satisfying lunch'
+    },
     instant_energy_restore: null,
     long_energy_restore: null,
     instant_hungry_restore: {
@@ -268,8 +316,8 @@ export default [
       en: "The old cracker"},
     duration: 30,
     description: {
-      ru: 'Жесть, сломал зубы',
-      en: 'Fuck, no insurance'
+      ru: 'Питательно и ужасно на вкус',
+      en: 'It is nutritious and tastes terrible'
     },
     instant_energy_restore: null,
     long_energy_restore: null,
