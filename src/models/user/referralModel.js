c797546f2f7ebb66a67fee37ee5557f9d101cc75
Leonid Brown <leonid.brown01@gmail.com>
2025-03-24 23:26:33 +0700

    fix: ref model

---
+++ src/models/user/referralModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+
+const schema = new mongoose.Schema(
+  {
+    refer_id: {
+      type: Number,
+      required: true,
+    },
+    referral_id: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+)
+const Referal = mongoose.model("Referral", schema)
+export default Referal
