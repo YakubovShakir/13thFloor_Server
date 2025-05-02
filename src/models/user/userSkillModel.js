2ab9ff40021cb8202172086cf016e5ba11cbc91e
Leonid Brown <leonid.brown01@gmail.com>
2025-05-02 21:39:36 +0700

    fix

--- src/models/user/userSkillModel.js
+++ src/models/user/userSkillModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 const userSkillSchema = new mongoose.Schema({
   id: { type: Number, required: true },
   skill_id: { type: Number, required: true },
-})
+}, { timestamps: true })
 
 const UserSkill = mongoose.model("users_skills", userSkillSchema)
 
